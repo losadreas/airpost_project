96aa5b36be35c3c10c5fc6e455046053496c747f
Andrii Los <losadreas@gmail.com>
2022-09-03 15:39:07 +0300

    fixed models

--- airport/customer/models.py
+++ airport/customer/models.py
@@ -4,10 +4,9 @@ from django.db import models
 
 class Customer(AbstractUser):
     email = models.EmailField(unique=True)
-    bio = models.CharField(max_length=500, blank=True)
-    avatar = ResizedImageField(size=[1000, None], upload_to='account/avatars/%y/%m/%d', null=True, blank=True)
+    ticket = models.CharField(max_length=500, blank=True)
     token = models.CharField(max_length=124, blank=True)
-    balance = models.PositiveIntegerFieldField(default=True)
+    balance = models.PositiveIntegerField()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
@@ -16,19 +15,20 @@ class Customer(AbstractUser):
 
 
 class Flight(models.Model):
-    destination = models.CharField(_("description"), max_length=150, blank=True)
+    destination = models.CharField(max_length=150, blank=True)
     date_flight = models.DateTimeField(_("date posted"), default=timezone.now)
     tags = models.CharField(_("tags"), max_length=150, blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+    passenger = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes_post')
     quantity_likes = models.PositiveIntegerField(default=0)
 
 
 class Ticket(models.Model):
-    passenger = models.CharField
-    flight = models.ForeignKey()
+    flight = models.ForeignKey(Flight)
+    passenger = models.ForeignKey(Customer)
+    price = models.PositiveIntegerField
     check_in = models.BooleanField
     luggage = models.PositiveIntegerField
     option = models.PositiveIntegerField
-
+    seat_type = models.Choices
