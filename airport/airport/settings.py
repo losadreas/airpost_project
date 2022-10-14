c42a8832d8d72719f3494cdac83305b84c2ce8fd
losadreas <losadreas@gmail.com>
2022-10-14 14:54:13 +0300

    Hided passwords 2

--- airport/airport/settings.py
+++ airport/airport/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#4_praw64ek%bbq#%#&op9p0&hnzb@v7e5@=pe=kd$yzut=&2'
+SECRET_KEY = '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
